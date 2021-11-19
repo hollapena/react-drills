8bd9c8e97edc0bd5295e6ad0cc4f99e1607e8acf
Callen Schultz <callen.schultz@gmail.com>
2021-11-19 16:22:15 -0600

    App 3 completed

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -2,25 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component{
+constructor(){
+  super();
+  this.state={
+    dataArray:['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'],
+    userInput:''
+  }
+};
+
+listMaker(userInput){
+  userInput=userInput.toLowerCase();
+  let answer;
+  let arr = this.state.dataArray
+  let filteredArray=[];
+  if(userInput !== ''){
+    for(let i = 0; i<arr.length; i++){
+      if(arr[i].toLowerCase().includes(userInput)){
+        filteredArray.push(arr[i])
+      }
+    }answer = filteredArray.map((state) => <li>{state}</li>)
+  } else {
+  answer = arr.map((state) => <li>{state}</li>)
+  }
+  return answer;
+}
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>State Name Finder</h1>
+      <input onChange={(e) => this.setState({userInput:e.target.value})}></input>
+      <ul className="list">{this.listMaker(this.state.userInput)}</ul>
     </div>
   );
+};
 }
 
 export default App;
