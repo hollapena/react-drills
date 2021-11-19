50f1ca36e9402e7ee59cf5c47f2faf723737b32f
Callen Schultz <callen.schultz@gmail.com>
2021-11-19 15:48:27 -0600

    App 2 Completed

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -2,25 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component{
+constructor(){
+  super();
+  this.state={
+    dataArray:['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+  };
+};
+
+listItems (arr){
+  let display=arr.map((state) => <li>{state}</li>);
+  return display;
+};
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1 className="title">Alphabetical List of States</h1>
+    <ul className="stateList">
+      {this.listItems(this.state.dataArray)}
+    </ul>
     </div>
   );
-}
+};
+};
 
 export default App;
